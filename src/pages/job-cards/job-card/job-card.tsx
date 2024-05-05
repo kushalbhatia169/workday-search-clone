98fa9a56108a12c016eb9dedadc362869eca30d9
kushalbhatia169 <kushalbhatia169@gmail.com>
2024-05-05 17:41:20 +0530

    styling added to job card header and estimated salary sections

--- src/pages/job-cards/job-card/job-card.tsx
+++ src/pages/job-cards/job-card/job-card.tsx
@@ -6,26 +6,32 @@ import Typography from '@mui/material/Typography/Typography';
 import NoImage from "../../../assets/No_image_available.svg";
 import { JobCardData } from '../../../types/job-cards.types';
 import styles from "./job-card.module.css";
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import { Avatar } from '@mui/material';
 
 const JobCard = (props: JobCardData) => {
 
   return (
     <Card className={styles.card}>
-      <div className={styles.header}>
+      <div className={`${styles.header} ms-2`}>
         <div className={styles.imageDiv}>
-          <img className={styles.media} src={props.logoUrl || NoImage} title={props.companyName || ""} height={64} width={64}/>
+          <img className={styles.media} src={props.logoUrl || NoImage} title={props.companyName || ""} height={64} width={40}/>
         </div>
-        <div className={styles.overlay}>
-          <Typography variant="h6">{props.companyName}</Typography>
-          <Typography variant="body2">{props.jobRole}</Typography>
-          <Typography variant="body2">{props.location}</Typography>
+        <div className={`${styles.overlay} mt-2`}>
+          <Typography variant="h3">{props.companyName}</Typography>
+          <Typography variant="h2">{props.jobRole}</Typography>
+          <div>
+            <Typography variant="body2" className={styles.locationText}>{props.location}</Typography>
+          </div>
         </div>
       </div>
       <div className={styles.details}>
-        <Typography>Estimated Salary:</Typography>
-        <Typography variant="body2" color="textSecondary" component="span">{props.maxJdSalary}</Typography>
-        <Typography variant="body2" color="textSecondary" component="span">{props.minJdSalary}</Typography> LPA
+        <Typography variant="body2" className={styles.estimatedSalary}>
+          Estimated Salary: {props.salaryCurrencyCode === "USD" ? "$" : props.salaryCurrencyCode}
+            &nbsp;{props.maxJdSalary} {props.minJdSalary && `- ${props.minJdSalary}`} LPA &nbsp;
+          <CheckBoxIcon htmlColor='green'/>
+        </Typography>
+
       </div>
       <Typography gutterBottom variant="h5" component="h2">About Company</Typography>
       <CardContent>
